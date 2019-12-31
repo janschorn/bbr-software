a133dbcfa4d26effeea2e3bc9ee9138df5830672
Jan Schorn <schorn@bluehende-landschaft.de>
2019-12-31 19:14:36 +0100

    Custom Modules: Icons Updated, Language Strings

---
+++ SuiteCRM/custom/Extensions/modules/Accounts/Language/de_DE.BBR-custom.php
@@ -0,0 +1,4 @@
+ 
+<?php
+
+$mod_strings['LBL_BBR_SOWINGS_ACCOUNTS_FROM_BBR_SOWINGS_TITLE'] = 'Ansaaten';
---
+++ SuiteCRM/custom/Extensions/modules/Contacts/Language/de_DE.BBR-custom.php
@@ -0,0 +1,5 @@
+ 
+<?php
+
+$mod_strings['LBL_BBR_SOWINGS_CONTACTS_FROM_BBR_SOWINGS_TITLE'] = 'Ansaaten';
+$mod_strings['LBL_BBR_SIGNS_CONTACTS_FROM_BBR_SIGNS_TITLE'] = 'Schilder';
